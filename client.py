637ff631d1e1fb7379e60be6b0421ba79a338c80
shreyas <shreyasgoenka19@gmail.com>
2021-03-27 16:43:29 +0530

    added multithreaded cpu load

--- client.py
+++ client.py
@@ -1,12 +1,40 @@
 import socket
+import time
 
-try: 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-except socket.error as err: 
-    print ("socket creation failed with error %s" %(err))
+def testClient():
+    try: 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    except socket.error as err: 
+        print ("socket creation failed with error %s" %(err))
 
 
-server_ip = '192.168.123.165'
-port = 5555
+    server_ip = '192.168.123.165'
+    port = 5555
 
-s.connect((server_ip, port))
+    s.connect((server_ip, port))
+
+def loadServer(s, num_iters, val):
+    for i in range(num_iters):
+        s.send(val.encode())
+
+
+def main():
+    try: 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    except socket.error as err: 
+        print ("socket creation failed with error %s" %(err))
+
+
+    server_ip = '192.168.123.165'
+    port = 5555
+
+    s.connect((server_ip, port))
+
+    for i in range(60):
+        loadServer(s, 1000, 10000)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
+
+
--- server.py
+++ server.py
@@ -1,18 +1,68 @@
-import socket             
+import socket
+from _thread import *
+import threading  
+from threading import Thread
 
-s = socket.socket()         
-port = 5555
-
-s.bind(('', port))
-print("server socket bound")
-
-s.listen()
-
-c, addr = s.accept()
-print("Got connection from", addr)
-c.close()
+def computeSequenceSum(x):
+    ans = 0
+    for i in range(0, int(x)+1):
+        ans += i
+    return ans
 
 
+def ClientThread(Thread):
 
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+
+    def run(self):
+        data = conn.recv(x)
+        # TODO: will maybe need to convert this byte string
+        print("Data: ", data)
+        computeSequenceSum(x)
+        conn.send("done compute")
+
+
+
+def multiThreadedServer():
+    host_ip = ""
+    port = 5555
+
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    s.bind((host_ip, port))
+
+    threads = []
+
+    while True:
+        s.listen(5)
+        conn, (ip, port) = s.accept()
+        computeThread = ClientThread(ip, port)
+        computeThread.start()
+        threads.append(computeThread)
+
+    for t in threads:
+        t.join()
+
+
+def singleThreadedServer():
+    s = socket.socket()         
+    port = 5555
+
+    s.bind(('', port))
+    s.listen()
+
+    c, addr = s.accept()
+
+    while True:
+        data = c.recv(1024).decode()
+        data = str(data)
+        thread = Thread(target=computeSequenceSum, args=(data, ))
+
+
+if __name__ == "__main__":
+    singleThreadedServer()
 
 
