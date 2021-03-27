43ce177e3573e3a85d64cb6eb23f0cff0810748f
s <shreyasgoenka19@gmail.com>
2021-03-27 16:49:57 +0000

    -

--- server.py
+++ server.py
@@ -53,7 +53,7 @@ def singleThreadedServer():
     s = socket.socket()         
     port = 5555
 
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     s.bind(('', port))
     s.listen()
